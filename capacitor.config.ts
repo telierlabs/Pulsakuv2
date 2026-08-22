d75e32228e200b4f8b5cfebff508f05c1cba4048
telierlabs <zixksuper@gmail.com>
2026-08-22 22:22:09 +0700

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.telierlabs.puslink',
+  appName: 'Puslink',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
